8b61ccc9afbc3e50f7d55ff82448466a4a92ffc0
Adam Richardson <adam@forestgiant.com>
2018-05-30 16:29:38 -0400

    Made some changes to my emacs config

--- emacs.el
+++ emacs.el
@@ -23,8 +23,74 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
-  '(package-selected-packages (quote (lua-mode typescript-mode go-playground))))
+ '(ansi-color-faces-vector
+	 [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+	 ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#657b83")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-enabled-themes (quote (wombat)))
+ '(fci-rule-color "#eee8d5")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+	 (--map
+		(solarized-color-blend it "#fdf6e3" 0.25)
+		(quote
+		 ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#586e75")
+ '(highlight-tail-colors
+	 (quote
+		(("#eee8d5" . 0)
+		 ("#B4C342" . 20)
+		 ("#69CABF" . 30)
+		 ("#69B7F0" . 50)
+		 ("#DEB542" . 60)
+		 ("#F2804F" . 70)
+		 ("#F771AC" . 85)
+		 ("#eee8d5" . 100))))
+ '(hl-bg-colors
+	 (quote
+		("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342")))
+ '(hl-fg-colors
+	 (quote
+		("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
+ '(magit-diff-use-overlays nil)
+ '(package-selected-packages
+	 (quote
+		(yaml-mode exec-path-from-shell lua-mode typescript-mode go-playground)))
+ '(pos-tip-background-color "#eee8d5")
+ '(pos-tip-foreground-color "#586e75")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
+ '(term-default-bg-color "#fdf6e3")
+ '(term-default-fg-color "#657b83")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+	 (quote
+		((20 . "#dc322f")
+		 (40 . "#c85d17")
+		 (60 . "#be730b")
+		 (80 . "#b58900")
+		 (100 . "#a58e00")
+		 (120 . "#9d9100")
+		 (140 . "#959300")
+		 (160 . "#8d9600")
+		 (180 . "#859900")
+		 (200 . "#669b32")
+		 (220 . "#579d4c")
+		 (240 . "#489e65")
+		 (260 . "#399f7e")
+		 (280 . "#2aa198")
+		 (300 . "#2898af")
+		 (320 . "#2793ba")
+		 (340 . "#268fc6")
+		 (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+	 (quote
+		(unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496"))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -63,16 +129,12 @@
 			(setq initial-frame-alist
 						'(
 							(tool-bar-lines . 0)
-							(width . 200) ; chars
-							(height . 68) ; lines
-							;;
+							(fullscreen . maximized)
 							))
 			(setq default-frame-alist
 						'(
 							(tool-bar-lines . 0)
-							(width . 200)
-							(height . 68)
-							;;
+							(fullscreen . maximized)
 							)))
 	(progn
 		(setq initial-frame-alist
@@ -83,4 +145,13 @@
 						(tool-bar-lines . 0)))))
 
 ;; Load solarized
-(load-theme 'solarized-light t)
+;; (load-theme 'solarized-light t)
+
+;; Initial Buffer
+(setq initial-buffer-choice "~/code")
+
+;; Exec path from shell
+(exec-path-from-shell-initialize)
+
+;; F1 for bookmark-list
+(global-set-key (kbd "<f1>") 'list-bookmarks)
