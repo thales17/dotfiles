7ca1d1c45857a2ce0c1ca34d785b5fdd9d30f9bb
Adam Richardson <ajrich01@gmail.com>
2022-01-05 20:53:46 -0500

    Updated formatting for webpage

--- build-site.el
+++ build-site.el
@@ -1,6 +1,26 @@
+;; Setup straight.el
+(defvar bootstrap-version)
+  (let ((bootstrap-file
+	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+	(bootstrap-version 5))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+	  (url-retrieve-synchronously
+	   "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	   'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+
+(setq straight-base-dir ".packages/")
+
+(straight-use-package 'htmlize)
+
 (require 'ox-publish)
 
 (setq org-html-validation-link nil)
+
+(setq org-html-htmlize-output-type 'css)
 
 (setq org-publish-project-alist
       (list
