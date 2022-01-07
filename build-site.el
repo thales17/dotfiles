c9142a902360955b1b934d02226369cdbc9bb5fd
Adam Richardson <ajrich01@gmail.com>
2022-01-06 20:12:42 -0500

    Added sample setting css in org export

--- build-site.el
+++ build-site.el
@@ -1,16 +1,16 @@
 ;; Setup straight.el
 (defvar bootstrap-version)
-  (let ((bootstrap-file
-	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-	(bootstrap-version 5))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-	  (url-retrieve-synchronously
-	   "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	   'silent 'inhibit-cookies)
-	(goto-char (point-max))
-	(eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 (setq straight-base-dir ".packages/")
 
@@ -19,6 +19,17 @@
 (require 'ox-publish)
 
 (setq org-html-validation-link nil)
+
+(setq org-html-style
+      (concat "<style>"
+	      ".org-doc { color: blue; }"
+	      ".org-keyword { color: red; }"
+	      ".org-string { color: green; }"
+	      ".org-constant { color: purple; }"
+	      ".org-comment { color: darkcyan; }"
+	      ".org-type { color: orange; font-weight: bold; }"
+	      ".org-warning { color: red; }"
+	      "</style>"))
 
 (setq org-html-htmlize-output-type 'css)
 
